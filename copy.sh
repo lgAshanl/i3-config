384253c230e314e628cf11dc559526effa236e03
Unknown <igoshaaaaaa@gmail.com>
2017-09-07 22:39:26 +0300

    installing

--- copy.sh
+++ copy.sh
@@ -2,15 +2,15 @@
 
 git clone https://github.com/lgAshanl/i3-config
 
+name=$(whoami)
 path="/home/$name/.config/i3"
 git_path="./i3-config"
 version=$(cat $git_path'/v')
-name=$(whoami)
 if [ -f "$path/v" ]; then
    echo "File $FILE exists."
-	local_version=$(cat $path'/v')
+	$local_version=$(cat $path'/v')
 else
-   local_version='0'
+   $local_version='0'
 fi
 echo $version
 echo $local_version
@@ -23,3 +23,8 @@ if [ $version > $local_version ]; then
 fi
 
 rm -r $git_path
+
+sudo pacman -S polkit-gnome gnome-keyring -y
+sudo pacman -S alsa-utils lm_sensors gperftools powerline-fonts feh dmenu -y
+yaourt -S speedtest-cli ttf-font-awesome i3status-rust-git -y
+
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name=$(whoami)
+
+#sudo pacman -Suy -y
+#sudo pacman -S breeze-gtk breeze-icons
+#sudo pacman -S audacious audacious-plugins gedit gedit-plugins rust fish
+#yaourt -S maia-gtk-theme google-chrome 
+
+sudo chsh -s /usr/bin/fish $name
+
+sudo ./copy.sh
