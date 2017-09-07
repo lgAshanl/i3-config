08ceed4ca259494613797e60c65a2c7490285d5c
Unknown <igoshaaaaaa@gmail.com>
2017-09-07 23:24:12 +0300

    good test

--- copy.sh
+++ copy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-git clone https://github.com/lgAshanl/i3-config
+#git clone https://github.com/lgAshanl/i3-config
 
-#name=$(whoami)
+name=$(whoami)
 path="/home/$name/.config/i3"
 git_path="./i3-config"
 version=$(cat $git_path'/v')
 if [ -f "$path/v" ]; then
-   echo "File $FILE exists."
+	echo "File $FILE exists."
 	local_version=$(cat $path'/v')
 else
-   local_version=0
+	local_version=0
 fi
-echo $version
-echo $local_version
+echo "to " $version
+echo "from " $local_version
 
-if [ $version > $local_version ]; then
+if [[ "$version" > "$local_version" ]]; then
 	cp -rf $git_path/config $path
 	cp -rf $git_path/status.toml $path
 	cp -rf $git_path/copy.sh $path
@@ -24,7 +24,7 @@ fi
 
 sudo rm -r $git_path
 
-#sudo pacman -S polkit-gnome gnome-keyring -y
-#sudo pacman -S alsa-utils lm_sensors gperftools powerline-fonts feh dmenu -y
-#yaourt -S speedtest-cli ttf-font-awesome i3status-rust-git -y
+sudo pacman -S polkit-gnome gnome-keyring -y
+sudo pacman -S alsa-utils lm_sensors gperftools powerline-fonts feh dmenu -y
+yaourt -S speedtest-cli ttf-font-awesome i3status-rust-git -y
 
--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 name=$(whoami)
 
-#sudo pacman -Suy -y
-#sudo pacman -S breeze-gtk breeze-icons
-#sudo pacman -S audacious audacious-plugins gedit gedit-plugins rust fish
-#yaourt -S maia-gtk-theme google-chrome 
+sudo pacman -Suy -y
+sudo pacman -S breeze-gtk breeze-icons -y
+sudo pacman -S audacious audacious-plugins gedit gedit-plugins rust fish -y
+yaourt -S maia-gtk-theme google-chrome -y
 
 sudo chsh -s /usr/bin/fish $name
 
