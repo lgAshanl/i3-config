c07bffa391728fa3d95c9f1ff7007f8b91d6db09
lgAshanl <igoshaaaaaa@gmail.com>
2017-12-24 03:30:01 +0000

    nvdock

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 name=$(whoami)
 
 sudo pacman -Suy -y
-sudo pacman -S breeze-gtk breeze-icons -y
+sudo pacman -S breeze-gtk breeze-icons nvdock -y
 sudo pacman -S audacious audacious-plugins gedit gedit-plugins rust fish -y
 yaourt -S maia-gtk-theme google-chrome -y
 
