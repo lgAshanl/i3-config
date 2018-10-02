5efa1ddabd8368abfbe6b11a5f63512278118589
Igor <igoshaaaaaa@gmail.com>
2018-10-02 19:00:34 +0300

    init: debian

--- copy.sh
+++ copy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# Dancing for good work i3
+
+dist=$(cat /etc/os-release | grep -o 'ID=\w*' | grep -oE 'arch|debian')
 
 git clone https://github.com/lgAshanl/i3-config
 
+# Config
 name=$(whoami)
 path="/home/$name/.config/i3"
 mkdir $path
@@ -25,7 +29,27 @@ fi
 
 sudo rm -r $git_path
 
-sudo pacman -S polkit-gnome gnome-keyring -y
-sudo pacman -S alsa-utils lm_sensors gperftools powerline-fonts feh dmenu -y
-yaourt -S speedtest-cli ttf-font-awesome i3status-rust-git -y
+# Packages
+if [[ $dist = 'arch' ]]; then
+	sudo pacman -S polkit-gnome gnome-keyring -y
+	sudo pacman -S alsa-utils lm_sensors gperftools powerline-fonts feh dmenu -y
+	yaourt -S speedtest-cli ttf-font-awesome i3status-rust-git -y
+elif [[ $dist = 'debian' ]]; then
+	sudo apt install gnome-keyring alsa-utils lm-sensors google-perftools fonts-powerline feh i3 speedtest-cli fonts-font-awesome -y
+	sudo apt install dpkg debconf debhelper lintian
+
+	cur_dir=$(pwd)
+	mkdir /tmp/i3-status-rust
+	cd /tmp/i3-status-rust
+
+	git clone https://github.com/greshake/i3status-rust
+	cd i3status-rust && cargo build --release
+	cp target/release/i3status-rs ~/bin/i3status-rs
+
+	cd $cur_dir
+	rm -rf /tmp/i3-status-rust
+else
+	echo "shit, unknown os"
+	exit 1
+fi
 
--- install.sh
+++ install.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 name=$(whoami)
+dist=$(cat /etc/os-release | grep -o 'ID=\w*' | grep -oE 'arch|debian')
+echo $dist
 
-sudo pacman -Suy -y
-sudo pacman -S breeze-gtk breeze-icons nvdock -y
-sudo pacman -S audacious audacious-plugins gedit gedit-plugins rust fish -y
-yaourt -S maia-gtk-theme google-chrome -y
+if [[ $dist = 'arch' ]]; then
+	sudo pacman -Syu -y
+	sudo pacman -S breeze-gtk breeze-icons -y
+	sudo pacman -S audacious audacious-plugins gedit gedit-plugins rustup fish -y
+	yaourt -S maia-gtk-theme google-chrome -y
+elif [[ $dist = 'debian' ]]; then
+	cur_dir=$(pwd)
+	mkdir /tmp/init_installation
+	cd /tmp/init_installation
+
+	# Full update
+	sudo apt update -y -f
+	sudo apt dist-upgrade -y
+	sudo apt update -y -f -q
+	sudo apt upgrade -y
+
+	# Base packages
+	sudo apt install gtk3-engines-breeze breeze-icon-theme audacious audacious-plugins gedit gedit-plugins fish -y
+	
+	# Browser
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	sudo dpkg -i google-chrome-stable_current_amd64.deb
+	rm google-chrome-stable_current_amd64.deb
+
+	# The best language
+	curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly
+	rustup completions bash > /etc/bash_completion.d/rustup.bash-completion
+	rustup completions fish > ~/.config/fish/completions/rustup.fish
+	rm sh.rustup.rs
+
+	cd $cur_dir
+	rm -rf /tmp/init_installation
+else
+	echo "shit, unknown os"
+	exit 1
+fi
 
 sudo chsh -s /usr/bin/fish $name
 
